6267fec23bc2e8eb081d4c857bcdbf18ae851e91
User <paadams@nmu.edu>
2017-07-07 14:39:18 -0400

    first commit

---
+++ week-one/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to week-one
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ week-one/test/week_one/core_test.clj
@@ -0,0 +1,7 @@
+(ns week-one.core-test
+  (:require [clojure.test :refer :all]
+            [week-one.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
