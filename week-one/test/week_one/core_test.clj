baf66417b0606ec2e91bbf76d06c357d8ec76a91
User <paadams@nmu.edu>
2017-07-09 00:41:43 -0400

    added tests

--- week-one/test/week_one/core_test.clj
+++ week-one/test/week_one/core_test.clj
@@ -9,3 +9,23 @@
 (deftest prob-one
   (testing "problem #1."
     (is (= true true))))
+
+(deftest prob-three
+  (testing "problem #3."
+    (is
+     (= "HELLO WORLD" (.toUpperCase "hello world")))))
+
+(deftest prob-four
+  (testing "problem #4."
+    (is
+     (= (list :a :b :c) '(:a :b :c)))))
+
+(deftest prob-five-a
+  (testing "problem #5.A"
+    (is
+     (= '(1 2 3 4) (conj '(2 3 4) 1)))))
+
+(deftest prob-five-b
+  (testing "problem 5.B"
+    (is
+     (= '(1 2 3 4) (conj '(3 4) 2 1)))))
