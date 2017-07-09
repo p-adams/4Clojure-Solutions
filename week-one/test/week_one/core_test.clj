975370e537a77f69dd99d2a60a489d5a3ef0dc10
User <paadams@nmu.edu>
2017-07-09 11:02:22 -0400

    added additional tests

--- week-one/test/week_one/core_test.clj
+++ week-one/test/week_one/core_test.clj
@@ -1,6 +1,7 @@
 (ns week-one.core-test
   (:require [clojure.test :refer :all]
-            [week-one.core :refer :all]))
+            [week-one.core :refer :all]
+            [clojure.set]))
 
 (deftest a-test
   (testing "FIXME, I fail."
@@ -29,3 +30,39 @@
   (testing "problem 5.B"
     (is
      (= '(1 2 3 4) (conj '(3 4) 2 1)))))
+
+(deftest prob-six-a
+  (testing "problem 6.A"
+    (is
+     (= [:a :b :c] (list :a :b :c)))))
+
+(deftest prob-six-b
+  (testing "problem 6.B"
+    (is
+     (= [:a :b :c] (vec '(:a :b :c))))))
+
+(deftest prob-six-c
+  (testing "problem 6.C"
+    (is
+     (= [:a :b :c] (vector :a :b :c)))))
+
+
+(deftest prob-seven-a
+  (testing "problem 7.A"
+    (is
+     (= [1 2 3 4] (conj [1 2 3] 4)))))
+
+(deftest prob-seven-b
+  (testing "problem 7.B"
+    (is
+     (= [1 2 3 4] (conj [1 2] 3 4)))))
+
+(deftest prob-eight-a
+  (testing "problem 8.A"
+    (is
+     (= #{:a :b :c :d} (set '(:a :a :b :c :c :c :d :d))))))
+
+(deftest prob-eight-b
+  (testing "problem 8.B"
+    (is
+     (= #{:a :b :c :d} (clojure.set/union #{:a :b :c} #{:b :c :d})))))
